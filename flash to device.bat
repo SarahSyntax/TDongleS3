7d53f6fa69dc7067c5d8713995b2518f98ae1bfc
Sarah <127451212+SarahSyntax@users.noreply.github.com>
2025-05-04 17:25:49 -0500

    Update flash to device.bat

--- flash to device.bat
+++ flash to device.bat
@@ -1,7 +1,7 @@
 :start
 esptool.exe --chip esp32s3   --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size 16MB 0x0 firmware.bin
 
-@echo Press any key,keey find device praogram
+@echo Press any key, key finds device program
 pause
 goto start
 
